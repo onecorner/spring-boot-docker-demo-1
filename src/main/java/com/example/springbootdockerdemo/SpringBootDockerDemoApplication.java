e72b18df9ca4123302a80e4871a358fc1f716f67
hou.tianjin <6027022134@iwhalecloud.com>
2019-08-25 07:40:36 +0800

    Initial commit

---
+++ src/main/java/com/example/springbootdockerdemo/SpringBootDockerDemoApplication.java
@@ -0,0 +1,26 @@
+package com.example.springbootdockerdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class SpringBootDockerDemoApplication {
+
+    @RestController
+    @RequestMapping("/api")
+    public static class RestDemo {
+
+        @GetMapping("/test")
+        public String test() {
+            return "hello world";
+        }
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDockerDemoApplication.class, args);
+    }
+
+}
